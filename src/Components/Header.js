d95ce115431bf89664615deea2ee0c1ce1742391
Olle Ristenstrand <olelwow@hotmail.com>
2023-11-16 19:17:26 +0100

    Lagt till huvudmenyns utseende på inloggningssidan

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+
+const Header = () => {
+  return (
+    <AppBar id="headerBar">
+      <h1>WackBook</h1>
+    </AppBar>
+  );
+};
+
+export default Header;
