238b802da98e4f55d06bf6fbab1ab4dc173fd108
Olle Ristenstrand <olelwow@hotmail.com>
2023-11-17 11:01:29 +0100

    Fixat typo i SignUp.js

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -38,7 +38,7 @@ export default function SignUp() {
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
-            Logga in
+            Registrera nytt konto
           </Typography>
           <Box
             component="form"
