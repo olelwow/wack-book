a28d89f1371bdcb07b65688d2b3846a45598e8c0
Jonas Öhrn <theeagle87@hotmail.com>
2023-11-17 14:21:01 +0100

    en början på funktion reg ny anv /Jonas

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -1,4 +1,4 @@
-import * as React from "react";
+
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
@@ -9,18 +9,42 @@ import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import React, { useState } from "react";
 
 const defaultTheme = createTheme();
 
 export default function SignUp() {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get("email"),
-      password: data.get("password"),
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    password: "",
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      ["firstName"]: value,
     });
   };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Now you can use formData to save or send the data as needed
+    console.log(formData);
+    // You can also perform additional actions here, such as sending the data to a server
+  };
+
+  // const handleSubmit = (event) => {
+  //   event.preventDefault();
+  //   const data = new FormData(event.currentTarget);
+  //   console.log({
+  //     FirstName: data.get("firstName"),
+  //     LastName: data.get("lastName"),
+  //     Password: data.get("signUpPassWord"),
+  //   });
+  //   console.log("Kollar vad som kommer ut i form" + data[1],);
+  // }
 
   return (
     <ThemeProvider theme={defaultTheme}>
@@ -42,7 +66,7 @@ export default function SignUp() {
           </Typography>
           <Box
             component="form"
-            onSubmit={handleSubmit}
+            onSubmit={handleSubmit}  
             noValidate
             sx={{ mt: 1 }}
           >
@@ -76,6 +100,7 @@ export default function SignUp() {
             <Button
               id="signUpButton"
               type="submit"
+              onClick={handleChange}
               fullWidth
               variant="contained"
               sx={{ mt: 3, mb: 2 }}
